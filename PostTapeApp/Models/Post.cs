831596927702d02f1382a43af4963856f5535bfc
Hihiz <public@mail.ru>
2023-07-03 20:30:38 +0300

    Добавьте файлы проекта.

---
+++ PostTapeApp/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostTapeApp.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+
+    [Display(Name = "Имя пользователя")]
+    public User? User { get; set; }
+
+    [Display(Name = "Дата публикации")]
+    [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+    public DateTime DatePublish { get; set; } = DateTime.Now;
+
+
+    [Display(Name = "Сообщение")]
+    public string Message { get; set; }
+
+}
---
+++ PostTapeApp/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostTapeApp.Models;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Имя")]
+    public string Name { get; set; }
+}
