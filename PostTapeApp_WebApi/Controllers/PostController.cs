81eb912455a2764c3006820c11e6409390b461d6
Hihiz <public@mail.ru>
2023-07-06 01:45:48 +0300

    Методы Get, Post, Put, Delete

--- PostTapeApp_WebApi/Controllers/PostController.cs
+++ PostTapeApp_WebApi/Controllers/PostController.cs
@@ -22,7 +22,7 @@ namespace PostTapeApp_WebApi.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Post>> GetId(int id)
         {
-            IQueryable post = _db.Posts.Include(u => u.User).Where(u => u.Id == id);
+            IQueryable<Post> post = _db.Posts.Include(u => u.User).Where(u => u.Id == id);
 
             if (post == null)
             {
@@ -30,6 +30,62 @@ namespace PostTapeApp_WebApi.Controllers
             }
 
             return Ok(post);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Post>> CreatePost(Post post)
+        {
+            _db.Posts.Add(post);
+            await _db.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Get), new { id = post.Id }, post);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Post>> UpdatePost(int id, Post request)
+        {
+            if (id != request.Id)
+            {
+                return BadRequest($"Id введен не верно");
+            }
+
+            Post post = await _db.Posts.FindAsync(id);
+
+            if (post == null)
+            {
+                return BadRequest($"Пост с Id: {id}, не найден");
+            }
+
+            post.UserId = request.UserId;
+            post.DatePublish = request.DatePublish;
+            post.Message = request.Message;
+
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+
+            return Ok(await _db.Posts.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Post>> DeletePost(int id)
+        {
+            Post post = await _db.Posts.FindAsync(id);
+
+            if (post == null)
+            {
+                return BadRequest("Пост не найден");
+            }
+
+            _db.Posts.Remove(post);
+            _db.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
